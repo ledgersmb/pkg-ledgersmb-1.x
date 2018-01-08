47e9d65c34c8ca89a172062dd6e7d278a31d10a6
Robert James Clay <jame@rocasa.us>
2018-01-07 23:30:37 -0500

    New upstream version 1.5.15

--- lib/LedgerSMB/Form.pm
+++ lib/LedgerSMB/Form.pm
@@ -181,8 +181,8 @@ sub new {
     #menubar will be deprecated, replaced with below
     $self->{lynx} = 1 if ( ( defined $self->{path} ) && ( $self->{path} =~ /lynx/i ) );
 
-    $self->{version}   = "1.5.14";
-    $self->{dbversion} = "1.5.14";
+    $self->{version}   = "1.5.15";
+    $self->{dbversion} = "1.5.15";
 
     bless $self, $type;
 
