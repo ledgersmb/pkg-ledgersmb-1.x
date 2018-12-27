be531ae3958915eb5a15dc647288a130ebfa92d7
Robert James Clay <jame@rocasa.us>
2018-12-27 07:52:50 -0500

    New upstream version 1.6.9

--- old/lib/LedgerSMB/Form.pm
+++ old/lib/LedgerSMB/Form.pm
@@ -180,8 +180,8 @@ sub new {
             split(/:/, $self->{cookie});
     }
 
-    $self->{version}   = "1.6.8";
-    $self->{dbversion} = "1.6.8";
+    $self->{version}   = "1.6.9";
+    $self->{dbversion} = "1.6.9";
 
     bless $self, $type;
 
